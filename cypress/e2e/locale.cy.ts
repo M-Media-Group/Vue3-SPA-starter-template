60458da75fd8a6bb55183880d86fa25ed03648cb
Michal <mwargan@gmail.com>
2022-11-01 18:43:40 +0100

    Added locale tests

---
+++ cypress/e2e/locale.cy.ts
@@ -0,0 +1,36 @@
+describe("Payments", () => {
+  it("Shows a language switcher in the footer", () => {
+    cy.visit("/");
+    cy.get("select[name=locales]").should("exist");
+    cy.get("footer").contains("English");
+    cy.get("footer").contains("Français");
+  });
+  it("The current selected locale matches the one in the HTML tag", () => {
+    cy.visit("/");
+    // Get the valuef rom "select[name=locales]"
+    cy.get("select[name=locales]").then(($select) => {
+      const value = $select.val();
+      // Get the lang attribute from the html tag
+      cy.get("html").should("have.attr", "lang", value);
+    });
+  });
+  it("Uses French by default if navigator language is French", () => {
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, "language", {
+          value: "fr-FR",
+        });
+      },
+    });
+    cy.get("select[name=locales]").should("have.value", "fr");
+  });
+  it("Actually changes the language when its changed", () => {
+    cy.visit("/");
+    cy.get("select[name=locales]").select("fr");
+    cy.get("footer").contains("Français");
+    // The lang attribute should be "fr"
+    cy.get("html").should("have.attr", "lang", "fr");
+    // The navbar should contain "Connexion"
+    cy.get("nav").should("contain", "Connexion");
+  });
+});
