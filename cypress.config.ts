5ea85b4c7d1c95813324216197ffb0dfc74d9e47
Michal <mwargan@gmail.com>
2022-11-03 08:43:04 +0100

    Switched to default testing mobile view in Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
+    // Viewport for mobile
+    viewportWidth: 375,
   },
 });
