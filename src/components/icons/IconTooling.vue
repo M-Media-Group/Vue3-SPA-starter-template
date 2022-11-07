0dead00310e2ed5dc5e62f420d3a292460ca49af
Michal <mwargan@gmail.com>
2022-11-07 21:37:56 +0100

    Removed unused class

--- src/components/icons/IconTooling.vue
+++ src/components/icons/IconTooling.vue
@@ -5,7 +5,6 @@
     xmlns:xlink="http://www.w3.org/1999/xlink"
     aria-hidden="true"
     role="img"
-    class="iconify iconify--mdi"
     width="24"
     height="24"
     preserveAspectRatio="xMidYMid meet"
