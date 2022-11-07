aae959ca3b2c037d3f4697e65bba81640f0220bd
Michal <mwargan@gmail.com>
2022-11-07 15:45:11 +0100

    Added vue translation handling

---
+++ .vue-translation.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const { JSONAdapter } = require('vue-translation-manager')
+
+module.exports = {
+    srcPath: path.join(__dirname, 'src/'),
+    adapter: new JSONAdapter({ path: path.join(__dirname, 'src/locales/') }),
+    languages: ['en', 'fr']
+}
