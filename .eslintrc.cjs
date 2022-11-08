f062492e85bd9e22cc5dfc893749cbec72d223f9
Michal <mwargan@gmail.com>
2022-11-08 13:29:11 +0100

    Added unused imports check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,21 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: "latest",
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+  },
+  plugins: ["unused-imports"],
+  rules: {
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
 };
