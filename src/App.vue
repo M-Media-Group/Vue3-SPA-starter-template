7ac1d34a9f6103ad4434d62b30aecedaa4ba05c2
Michal <mwargan@gmail.com>
2022-11-11 18:08:38 +0100

    Add loading state while router resolving

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,30 @@
 import PageFooter from "./components/PageFooter.vue";
 import NavBar from "./components/NavBar.vue";
 import { useUserStore } from "./stores/user";
-import { RouterView } from "vue-router";
+import { RouterView, useRouter } from "vue-router";
+import { ref } from "vue";
 
 // Using the store, attempt to get the current user
 const user = useUserStore();
+
+const isReady = ref(false);
+
 if (!user.attemptedToFetchUser) {
   user.getUser();
 }
+
+const router = useRouter();
+
+router.isReady().then(() => {
+  isReady.value = true;
+});
 </script>
 
 <template>
   <NavBar />
   <main>
-    <RouterView />
+    <RouterView v-if="isReady" />
+    <article v-else :aria-busy="true"></article>
   </main>
   <PageFooter />
 </template>
