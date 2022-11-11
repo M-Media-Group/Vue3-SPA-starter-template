aa83b9031e97a0a53304e0e95dac79d95d2fa3ab
Michal <mwargan@gmail.com>
2022-11-11 16:26:53 +0100

    Added button to 404

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import BaseButton from "@/components/BaseButton.vue";
+</script>
 <template>
   <div>
     <h1>404</h1>
+    <BaseButton to="/">{{ $t("Home") }}</BaseButton>
   </div>
 </template>
