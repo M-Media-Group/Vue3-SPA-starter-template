26311ad5abe852c90a9ac0b7d799bbd20a082a9b
Michal <mwargan@gmail.com>
2022-11-11 13:47:56 +0100

    Switched to new syntax for auth middleware

--- src/router/middlewares/auth.ts
+++ src/router/middlewares/auth.ts
@@ -1,12 +1,28 @@
 import { useUserStore } from "@/stores/user";
+import type { RouteLocationRaw } from "vue-router";
+import baseMiddleware from "./baseMiddleware";
 
 /** A middleware that checks if the user is authenticated */
-export default async () => {
-  const store = useUserStore();
-  await store.isReady;
-  if (!store.isAuthenticated) {
+class auth extends baseMiddleware {
+  form = "LoginOrRegister";
+
+  async handle() {
+    const store = useUserStore();
+    await store.isReady;
+    if (!store.isAuthenticated) {
+      return this.fail();
+    }
+  }
+
+  route(): false | RouteLocationRaw {
     return {
       name: "login",
     };
   }
+}
+
+const middleware = new auth();
+
+export default (options: any) => {
+  return middleware.setOptions(options).handle();
 };
