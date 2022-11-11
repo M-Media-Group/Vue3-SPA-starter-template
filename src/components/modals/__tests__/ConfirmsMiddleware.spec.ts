0d4fab01870bc8d133a3a6b1024257f105ab8dad
Michal <mwargan@gmail.com>
2022-11-11 16:27:47 +0100

    Added base test for confirms middleware

---
+++ src/components/modals/__tests__/ConfirmsMiddleware.spec.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+
+import { RouterLinkStub, mount } from "@vue/test-utils";
+import BaseModal from "../BaseModal.vue";
+
+describe("Confirms Middleware", () => {
+  // Globally stub the router-link component
+  const globalStubs = {
+    RouterLink: RouterLinkStub,
+  };
+
+  it("renders a confirmation element", () => {
+    const wrapper = mount(BaseModal, {
+      props: {
+        title: "Test Title",
+        middleware: "auth",
+      },
+      slots: {
+        default: "<button name=test>Click me</button>",
+      },
+      global: {
+        stubs: globalStubs,
+      },
+    });
+
+    // Expect a dialog element to exist with the open attribute
+    expect(wrapper.find("dialog").exists()).toBe(true);
+
+    // Expect a article element to exist within the dialog element
+    expect(wrapper.find("dialog article").exists()).toBe(true);
+
+    // By default, the modal should not be open
+    expect(wrapper.find("dialog").attributes("open")).toBe(undefined);
+
+    // There should be a button element
+    expect(wrapper.find("button[name=test]").exists()).toBe(true);
+  });
+});
