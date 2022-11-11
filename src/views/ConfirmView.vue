02d3b4294d0c8dc0e6a800761918483302050aa9
Michal <mwargan@gmail.com>
2022-11-11 16:27:13 +0100

    Added redirect handling on success

--- src/views/ConfirmView.vue
+++ src/views/ConfirmView.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import CardElement from "@/components/CardElement.vue";
 import { defineAsyncComponent } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
 const element = defineAsyncComponent(
   () => import(`./../forms/${route.params.element}.vue`)
 );
+const redirect = () => {
+  // Redirect to the home page
+  router.push((router.currentRoute.value.query.redirect as string) ?? "/");
+};
 </script>
 <template>
   <CardElement title="Continue">
-    <component :is="element"></component>
+    <component :is="element" @success="redirect"></component>
   </CardElement>
 </template>
