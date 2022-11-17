75a041bfe1963ded9df9737e3982568d1af3306a
Michal <mwargan@gmail.com>
2022-11-17 17:35:03 +0100

    Remoed unused global mocks

--- unit.setup.ts
+++ unit.setup.ts
@@ -1,14 +1,8 @@
 // See also the vitest.config.ts file
 import { config } from "@vue/test-utils";
-import { SUPPORT_LOCALES, setupI18n } from "./src/locales/i18n";
+import { setupI18n } from "./src/locales/i18n";
 
 config.global.mocks = {
-  $t: (tKey) => tKey, // just return translation key
-  $i18n: {
-    locale: SUPPORT_LOCALES[0],
-    availableLocales: SUPPORT_LOCALES,
-    fallbackLocale: SUPPORT_LOCALES[0],
-  },
   navIsLoading: false,
 };
 
