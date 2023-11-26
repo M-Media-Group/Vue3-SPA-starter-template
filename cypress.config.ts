c4322d8368f0a3766d8425a11cbb4fdf71b6db71
Michal <mwargan@gmail.com>
2023-11-26 19:47:55 +0000

    ci: update calls to https

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    baseUrl: "https://localhost:4173",
     // Viewport for mobile
     viewportWidth: 375,
   },
