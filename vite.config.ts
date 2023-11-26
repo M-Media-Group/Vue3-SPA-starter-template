56776d82af9bcf615c3cec686ba8d7c7e35384f5
Michal <mwargan@gmail.com>
2023-11-26 16:08:12 +0000

    feat: ssl support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { URL, fileURLToPath } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    proxy: {},
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
