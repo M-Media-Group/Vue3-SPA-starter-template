953e3cd9605060825c0b942726ce14273e833ee4
Michal <mwargan@gmail.com>
2024-03-06 17:25:17 +0000

    fix: inconsistent behaviour of BaseButton disable state

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -7,16 +7,29 @@ defineProps({
     type: String,
     required: false,
   },
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
 });
 </script>
 <template>
-  <a role="button" v-if="$attrs.href">
+  <a
+    role="button"
+    v-if="$attrs.href"
+    :disabled="disabled ? disabled : undefined"
+  >
     <!-- @slot This is the text or content that is clickable and navigates. This is the slot inside the `<a>` tag if href is present. This component automatically sets the href or to attributes for navigation for you. It renders as an `<a>` tag with a `role=button` if its `href`, otherwise its a button if `to` is passed. -->
     <slot />
   </a>
 
   <router-link v-else-if="to" :to="to" custom v-slot="{ navigate }">
-    <button v-bind="$attrs" @click="navigate">
+    <button
+      v-bind="$attrs"
+      @click="navigate"
+      :disabled="disabled ? disabled : undefined"
+    >
       <!-- @slot This is the default slot which contains the text.  -->
       <slot />
     </button>
