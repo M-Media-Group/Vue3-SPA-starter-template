82e17cc19159041fb9e334a84ac965472fcdc9d0
Michal <mwargan@gmail.com>
2024-03-06 18:31:53 +0000

    fix: payment modal font size var

--- src/helpers/cssVariables.ts
+++ src/helpers/cssVariables.ts
@@ -1,14 +1,25 @@
 import { hsl2rgb } from "@/helpers/colors";
 
-export const getCssVarForStripe = (color: string) => {
-  const computedColor = getComputedStyle(document.documentElement)
-    .getPropertyValue(`--${color}`)
+export const getCssVarForStripe = (cssVariable: string) => {
+  const computedValue = getComputedStyle(document.documentElement)
+    .getPropertyValue(`--${cssVariable}`)
     .trim()
     .replace("deg", "");
 
+  // There's a bug with pico-css-font-size, so
+
+  // if the cssVariable is a percent, convert it to a number. If its higher than 100, we need to convert it into px units
+  if (computedValue.endsWith("%")) {
+    const percent = parseFloat(computedValue.replace("%", ""));
+    if (percent > 100) {
+      //  Just return the base size in pixels
+      return convertPercentToPixels(100) + "px";
+    }
+  }
+
   // If the computedColor starts with hsl, convert it to rgb
-  if (computedColor.startsWith("hsl")) {
-    const hsl = computedColor.replace("hsl(", "").replace(")", "").split(",");
+  if (computedValue.startsWith("hsl")) {
+    const hsl = computedValue.replace("hsl(", "").replace(")", "").split(",");
     const rgb = hsl2rgb(
       parseInt(hsl[0]),
       parseInt(hsl[1]) / 100,
@@ -18,5 +29,21 @@ export const getCssVarForStripe = (color: string) => {
     return `rgb(${rgbIn255Scale.join(",")})`;
   }
 
-  return computedColor;
+  return computedValue;
+};
+
+export const convertRemToPixels = (rem: number) => {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+};
+
+/**
+ * Based on the base fontSize, convert a percent value to pixels
+ * @param percent - The percent value to convert to pixels
+ * @returns The pixel value
+ */
+export const convertPercentToPixels = (percent: number) => {
+  return (
+    (percent / 100) *
+    parseFloat(getComputedStyle(document.documentElement).fontSize)
+  );
 };
