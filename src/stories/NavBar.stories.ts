da19984cb427cc4c96914f1143523b217e2cb8fc
Michal <mwargan@gmail.com>
2024-03-07 09:04:50 +0000

    chore: added story for open dropdown

--- src/stories/NavBar.stories.ts
+++ src/stories/NavBar.stories.ts
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import NavBar from "@/components/NavBar.vue";
 
 import { useUserStore } from "@/stores/user";
+import { expect, userEvent, within } from "@storybook/test";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<typeof NavBar> = {
@@ -29,6 +30,31 @@ export const LoggedIn: Story = {
     },
     template: "<NavBar />",
   }),
+};
+
+export const DropdownOpen: Story = {
+  render: () => ({
+    components: { NavBar },
+    setup() {
+      const user = useUserStore();
+      user.isAuthenticated = true;
+    },
+    template: "<NavBar />",
+  }),
+  play: async ({ canvasElement }: any) => {
+    const canvas = within(canvasElement);
+
+    // There should be no logout text visible. It will be in the dom, but not visible
+    expect(canvas.queryByText("Logout")).not.toBeVisible();
+
+    // First we click on the text "my account"
+    const myAccount = canvas.getByText("My Account");
+    // Click on the "My Account" text
+    await userEvent.click(myAccount);
+    // There should be a visible "Logout"
+    const logout = canvas.getByText("Logout");
+    expect(logout).toBeVisible();
+  },
 };
 
 export const Loading: Story = {
