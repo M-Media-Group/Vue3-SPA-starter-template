37852eb2637d6ddce767e4eb2ee1f0a7b96b7d29
Michal <mwargan@gmail.com>
2024-03-11 20:28:05 +0000

    chore: added html-validate to vscode

--- .htmlvalidate.js
+++ .htmlvalidate.js
@@ -10,5 +10,6 @@ module.exports = defineConfig({
   rules: {
     "prefer-native-element": "off", // needed for now for base-button, to review later
     "attribute-boolean-style": "off", // needed for now for base-button, to review later - it seems when looking in the browser that the attributes don't have any value, but the test fails saying they do
+    "element-case": "off",
   },
 });
