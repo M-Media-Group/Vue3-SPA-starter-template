ba5828008d4b69cbdc608a24111d7730db784480
Michal <mwargan@gmail.com>
2024-03-11 20:26:07 +0000

    chore: added html-validate to vscode

--- .htmlvalidate.js
+++ .htmlvalidate.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("html-validate");
 
 module.exports = defineConfig({
-  extends: ["html-validate:recommended"],
-    rules: {
-        "prefer-native-element": "off", // needed for now for base-button, to review later
-        "attribute-boolean-style": "off", // needed for now for base-button, to review later - it seems when looking in the browser that the attributes don't have any value, but the test fails saying they do
-    },
+  plugins: ["html-validate-vue"],
+  extends: ["html-validate:recommended", "html-validate-vue:recommended"],
+  elements: ["html5"],
+  "transform": {
+    "^.*\\.vue$": "html-validate-vue"
+  },
+  rules: {
+    "prefer-native-element": "off", // needed for now for base-button, to review later
+    "attribute-boolean-style": "off", // needed for now for base-button, to review later - it seems when looking in the browser that the attributes don't have any value, but the test fails saying they do
+  },
 });
