4182925be545a01d15937bd2eabfaa6e46ce0cae
Michal <mwargan@gmail.com>
2024-04-12 10:46:05 +0000

    fix: added advanced table example and fixed input margin

--- src/stories/Components/Table.stories.ts
+++ src/stories/Components/Table.stories.ts
@@ -63,3 +63,48 @@ export const Table: Story = {
     `,
   }),
 };
+
+// An advanced table where a row has the columns with a: checkbox, image, text-input, select, and a button
+export const AdvancedTable: Story = {
+  args: {
+    columnsToShow: 7,
+  },
+  render: ({ columnsToShow, rowsToShow, cellText, striped }) => ({
+    template: `
+      <table class="${striped ? "striped" : ""}">
+        <thead>
+            <tr>
+                ${Array(columnsToShow)
+                  .fill(null)
+                  .map(() => "<th>Column</th>")
+                  .join("")}
+            </tr>
+        </thead>
+        <tbody>
+            ${Array(rowsToShow)
+              .fill(null)
+              .map(
+                () => `
+                        <tr>
+                            <td><input type="checkbox"></td>
+                            <td><img src="https://via.placeholder.com/150"></td>
+                            <td><input type="text" value="${cellText}"></td>
+                            <td>
+                                <select>
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </td>
+                            <td><button>Click me</button></td>
+                            <td>153,000</td>
+                            <td><input type="checkbox" role="switch"></td>
+                        </tr>
+                    `
+              )
+              .join("")}
+        </tbody>
+        </table>
+    `,
+  }),
+};
