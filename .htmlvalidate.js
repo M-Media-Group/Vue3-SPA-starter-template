0d7a7416796515c83e5af900139167b035da69cc
Michal <mwargan@gmail.com>
2025-05-16 21:15:15 +0000

    tests: fix for html-validate

--- .htmlvalidate.js
+++ .htmlvalidate.js
@@ -1,12 +1,10 @@
-const { defineConfig } = require("html-validate");
+import { defineConfig } from "html-validate";
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: ["html-validate-vue"],
   extends: ["html-validate:recommended", "html-validate-vue:recommended"],
   elements: ["html5"],
-  "transform": {
-    "^.*\\.vue$": "html-validate-vue"
-  },
+
   rules: {
     "prefer-native-element": "off", // needed for now for base-button, to review later
     "attribute-boolean-style": "off", // needed for now for base-button, to review later - it seems when looking in the browser that the attributes don't have any value, but the test fails saying they do
